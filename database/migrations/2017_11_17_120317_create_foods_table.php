dd1234a48897aeb96b373e5133b480acd00f39b4
Yuya Aratani <hrgruri@gmail.com>
2017-11-23 10:11:42 +0900

    add migration files

---
+++ database/migrations/2017_11_17_120317_create_foods_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFoodsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('foods', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->float('calorie')->unsigned();
+            $table->float('red')->unsigned();
+            $table->float('green')->unsigned();
+            $table->float('yellow')->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('foods');
+    }
+}
