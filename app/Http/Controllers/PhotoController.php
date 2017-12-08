a0a8f22df31419eceed7b66310a99f4ee804174b
Yuya Aratani <hrgruri@gmail.com>
2017-12-08 14:38:10 +0900

    画像取得のチェックを修正

--- app/Http/Controllers/PhotoController.php
+++ app/Http/Controllers/PhotoController.php
@@ -3,13 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Photo;
 
 class PhotoController extends Controller
 {
     public function show(Request $request, string $filename)
     {
         $path = storage_path("app/public/photos/{$filename}");
-        if (\File::exists($path)) {
+        if (Photo::where('filename', $filename)->exists() && \File::exists($path)) {
             return response()->file($path);
         } else {
             abort(404);
